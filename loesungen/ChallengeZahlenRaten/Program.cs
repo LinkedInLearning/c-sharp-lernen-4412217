37844acbcf36792f3beec7cfbe91ea035af194ea
Martina Kraus <kraus.martina.m@googlemail.com>
2023-04-03 10:06:37 +0200

    hinzufuegen von loesung zu Kapitel 5

--- loesungen/ChallengeZahlenRaten/Program.cs
+++ loesungen/ChallengeZahlenRaten/Program.cs
@@ -5,21 +5,16 @@ Console.WriteLine("Lass uns 'Zahlen raten' spielen.");
 Console.WriteLine("Ich denke mir eine Zahl zwischen 0 und 20 aus.");
 Console.WriteLine("Schreibe deine Zahl in die Konsole, oder -1 um es zu beenden.");
 
-
 int guessNum = 0;
 int guessCount = 0;
 
-
 while (keepGoing)
 {
-
   Console.WriteLine("Rate mal!?");
   guessNum = Convert.ToInt32(Console.ReadLine());
 
   if (guessNum == -1)
   {
-
-
     Console.WriteLine("Oh schade. Ich habe an die Zahl " + theNumber + " gedacht.");
     keepGoing = false;
   }
@@ -33,7 +28,16 @@ while (keepGoing)
     }
     else
     {
-      Console.WriteLine("Nope, {0} than that.", guessNum < theNumber ? "higher" : "lower");
+      string compare;
+      if (guessNum < theNumber)
+      {
+        compare = "größer";
+      }
+      else
+      {
+        compare = "kleiner";
+      }
+      Console.WriteLine("Leider falsch. Meine Zahl ist " + compare + " als diese");
     }
   }
 }
